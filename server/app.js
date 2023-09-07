da665754a525c57809ec802caecaa46cde02046e
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2023-09-07 11:16:22 +0300

    feat(backend): Integrate CORS and Helmet middlewares
    This commit adds the CORS and Helmet middleware to enhance the security and HTTP request handling of the application. CORS enables cross-origin resource sharing, and Helmet helps secure the Express app by setting various HTTP headers. These middleware packages are essential for a more robust and secure application.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const e = require('express');
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
 const connectToDatabase = require('./config/database-connection');
 
 const app = express();
@@ -10,6 +12,10 @@ dotenv.config();
 
 // Establish database connection
 connectToDatabase();
+
+// Initial CORS and Helmet middlewares
+app.use(cors());
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.send('You are here!');
