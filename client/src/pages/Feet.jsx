a0a6a039f1be35749f82197ae77e68a90cf3f059
Aslıhan Alp <113977691+AslihanAlp@users.noreply.github.com>
2023-09-11 12:22:35 +0300

    Delete client directory

--- client/src/pages/Feet.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FeetPage = () => {
-  return (
-    <div>FeetPage</div>
-  )
-}
-
-export default FeetPage
--- client/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return(
-    <div className='container'>
-      home
-    </div>
-  )
-}
-
-export default Home;
