5703a7cd392f8292a0a79aba53f685319d2d22a2
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2023-09-14 23:37:41 +0300

    test(backend): Create a test MongoDB server with mongoDB Memory Server package
    "mongoDB Memory Server" package has been installed.
    
    A mongoDB memory server has been set up in "testDatabase.js" file, located in "/tests/" directory, and functions has been created to connect to in-memory mongoDB server and close the connection and clear all data from collections.

---
+++ server/tests/testDatabase.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+
+// Connect to the in-memory database.
+module.exports.connectToDatabase = async () => {
+
+    await mongod.start();
+    const uri = (await mongod).getUri();
+    const mongooseOptions = {
+        useNewUrlParser: true
+        };
+    await mongoose.connect(uri, mongooseOptions);
+}
+
+// Drop database, close the connection, and stop mongod.
+module.exports.closeDatabase = async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+}
+
+// Remove all the data for all db collections.
+module.exports.clearDatabase = async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        const collection = collections[key];
+        await collection.deleteMany();
+    }
+}
