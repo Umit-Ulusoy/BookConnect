914f3f7035e17071a88f6fc9acc0dbaf2eecd554
Aslihan Alp <aslhanalp@gmail.com>
2023-09-14 11:47:59 +0300

    feat(frontend): Add landing page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Login from "./pages/Login";
 import Timer from "./pages/Timer";
+import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <div>
-      <Timer/>
+      <Landing />
     </div>
-   
-        
   );
 }
 
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,37 @@
-
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <div>
+      <header>
+        <h2>BookConnect</h2>
+        <div>
+          <button aria-label="login">Login</button>
+          <button aria-label="sign up">Sign up</button>
+        </div>
+      </header>
+      <hr />
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quas
+          quia suscipit dicta nostrum consectetur exercitationem iusto tenetur
+          iure magnam.
+        </p>
+      </section>
+      <hr />
+      <main>
+        {/* Ana içerik buraya gelecek */}
+      </main>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo
+          porro rem dolore ut illum assumenda iusto possimus reiciendis
+          eveniet!
+        </p>
+      </section>
+      <footer>
+        <p>Copyright © 2023 - All right reserved by BookConnect</p>
+      </footer>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
